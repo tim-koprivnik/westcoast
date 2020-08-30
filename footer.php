ba2cc771e4797f2cd9d4a8da00d0c2f4b47ec285
Tim Koprivnik <t.koprivnik@gmail.com>
2020-08-30 18:09:56 +0200

    ...

--- footer.php
+++ footer.php
@@ -17,6 +17,59 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 <div class="wrapper" id="wrapper-footer">
 
+    <div class="footer-testimonial-holder">
+        <div class="container">
+            <div class="row">
+                <div class="offset-lg-2 col-lg-8 offset-lg-2 offset-1 col-10 offset-1">
+                    <div class="testimonial">
+                        <div class="stars">
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                        </div>
+                        <div class="quote">"Amazing customer service and very good price will be definitely getting another one through these guys when needed so much help thankyou."</div>
+                        <div class="author">Damon, facebook review</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="footer-contact-form-holder">
+        <div class="container">
+            <div class="row">
+                <div class="offset-1 col-10 offset-1">
+                    <div class="footer-contact-form">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-lg-7">
+                                    <div class="contact">
+                                        <h2>Want to know more?</h2>
+                                        <p>To find out more about our services please contact us using any of the methods below, or alternatively fill in the contact form and a representative will be in touch as soon as possible.</p>
+                                        <a href="tel:1800 927 831"><i class="fa fa-phone"></i> Call today 1800 927 831</a>
+                                        <a href="mailto:info@westcoastwaste.com.au"><i class="fa fa-envelope"></i> Email info@westcoastwaste.com.au</a>
+                                    </div>
+                                </div>
+                                <div class="col-lg-5">
+                                    <div class="form">
+                                        <?php echo do_shortcode('[contact-form-7 id="24" title="Footer Contact Form"]'); ?>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="copyright">
+        <div class="container">
+            © Copyright West Coast Waste 2020 <span>//</span> <a href="#">Terms & Conditions</a> <span>//</span> <a href="https://thedude23.github.io/tk_portfolio/" target="blank">Website by Tim Koprivnik</a> 
+        </div>
+    </div>
 
 </div><!-- wrapper end -->
 
